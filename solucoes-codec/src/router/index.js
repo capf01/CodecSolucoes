856dbcc84445b1466f107d73890d729cf3ca33f4
capf01 <cesaraugustopachecoferreira@gmail.com>
2025-02-09 17:30:40 -0300

    Páginas e Rotas criadas

---
+++ solucoes-codec/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/HomePage.vue') // rota para página home 
+  },
+  {
+    path: '/Sobre',
+    name: 'SobrePage',
+    component: () => import('@/views/SobrePage.vue') // rota para a página Sobre
+  },
+  {
+    path: '/Contato',
+    name: 'ContactPage',
+    component: () => import('@/views/ContactPage.vue') // rota para a página Contato
+  },
+  {
+    path: '/Portifolio',
+    name: 'PortifolioPage',
+    component: () => import('@/views/ContactPage.vue') // rota para a página Portifolio
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
